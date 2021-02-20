c07f06529a7e1d3e978c504f481dfb8ee1d7ae40
lo-maxwell <lo.maxwell8240@gmail.com>
2021-02-20 10:50:26 -0800

    Working single flashcard

--- app/src/main/java/com/example/codepath_flashcardapp/MainActivity.java
+++ app/src/main/java/com/example/codepath_flashcardapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.codepath_flashcardapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 
 
 //Flashcard app for codepath intro to programming course
@@ -12,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.textView_Flashcard_Answer).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.textView_Flashcard_Question).setVisibility(View.VISIBLE);
+                findViewById(R.id.textView_Flashcard_Answer).setVisibility(View.INVISIBLE);
+            }
+        });
+
+        findViewById(R.id.textView_Flashcard_Question).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.textView_Flashcard_Answer).setVisibility(View.VISIBLE);
+                findViewById(R.id.textView_Flashcard_Question).setVisibility(View.INVISIBLE);
+            }
+        });
     }
+
 }
